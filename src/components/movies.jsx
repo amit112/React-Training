6766b9f8dd9f580c85f7b14097db5e7572228da9
amitm <akharadi.meena67@gmail.com>
2019-12-20 22:41:57 +0530

    Composing Components Part-2 -Done

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies} from '../services/fakeMovieService';
+import Like from './common/like';
 
 export class Movies extends Component {
     state = {
@@ -9,6 +10,12 @@ export class Movies extends Component {
       const movies =  this.state.movies.filter(m=> m._id !== movie._id);
       this.setState({movies});
     }
+    handleLike = movie => {
+        const movies =  [...this.state.movies];
+        const index =  this.state.movies.indexOf(movie);
+        movies[index].liked =!movies[index].liked;
+        this.setState({movies});
+      }
     render() {
         const  {length: count} =this.state.movies;
         if ( count === 0) 
@@ -16,7 +23,7 @@ export class Movies extends Component {
         return (
             <React.Fragment>
                 <p>Showing {count} movies in the database.</p>
-                <table className="table"><thead><tr><th>Title</th><th>Genre</th><th>Stock</th><th>Rate</th><th></th></tr>
+                <table className="table"><thead><tr><th>Title</th><th>Genre</th><th>Stock</th><th>Rate</th><th></th><th></th></tr>
                 </thead>
                 <tbody>
                     { this.state.movies.map(m => <tr key={m._id}>
@@ -24,6 +31,7 @@ export class Movies extends Component {
                         <td>{m.genre.name}</td>
                         <td>{m.numberInStock}</td>
                         <td>{m.dailyRentalRate}</td>
+                        <td><Like liked= {m.liked} onClick={()=> this.handleLike(m)} ></Like></td>
                         <td><button onClick={()=> this.handleDelete(m) } className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>) }
                 </tbody>
